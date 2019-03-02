fa9145d6cb781b09a783fea1e778b0f09df6313e
Angel <mcinegas@gmail.com>
2019-03-02 18:36:13 +0100

    creo botón activar/desactivar(muestras u ocultas la password, pipe funcionando

--- pipes-angular/src/app/pipe/capitalizar-nombre.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { CapitalizarNombrePipe } from './capitalizar-nombre.pipe';
-
-describe('CapitalizarNombrePipe', () => {
-  it('create an instance', () => {
-    const pipe = new CapitalizarNombrePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
